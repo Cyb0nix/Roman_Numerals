f2ced61c7e5f849fb0c810d9b787452bf7b789e4
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2023-06-25 01:19:29 +0200

    BasicTest pass

--- src/main/java/org/example/RomainConverter.java
+++ src/main/java/org/example/RomainConverter.java
@@ -3,6 +3,24 @@ package org.example;
 public class RomainConverter {
     public String convertToRomanNumber(int number){
 
+        if(number == 1){
+            return "I";
+        }
+        if(number == 2){
+            return "II";
+        }
+        if(number == 3){
+            return "III";
+        }
+        if(number == 4){
+            return "IV";
+        }
+        if(number == 5){
+            return "V";
+        }
+        if(number == 6){
+            return "VI";
+        }
 
         return null;
     }
