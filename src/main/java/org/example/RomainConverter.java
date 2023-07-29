a7d2a30798787a78090a02209b56a3cf2772d316
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2023-07-29 22:52:18 +0200

    Romain converter refactor

--- src/main/java/org/example/RomainConverter.java
+++ src/main/java/org/example/RomainConverter.java
@@ -1,100 +1,37 @@
 package org.example;
 
 public class RomainConverter {
-    public String convertToRomanNumber(int number){
 
-        if(number < 1){
+    private static final int[] VALUES = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    private static final String[] SYMBOLS = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+    public String toRomanNumerals(int number) {
+
+        if (number < 1) {
             throw new IllegalArgumentException("Number too small");
-        }else{
-            if(number < 4){
-                return convertOneToFour(number);
-            }
-            if(number == 4){
-                return "IV";
-            }
-            if(number == 5){
-                return "V";
-            }
-            if(number > 5 && number < 9){
-                return convertFiveToNine(number);
-            }
-            if(number == 9){
-                return "IX";
-            }
-            if(number == 10){
-                return "X";
-            }
-            if(number > 10 && number < 40){
-                return "X" + convertToRomanNumber(number - 10);
-            }
-            if(number == 40){
-                return "XL";
-            }
-            if(number > 40 && number < 50){
-                return "XL" + convertToRomanNumber(number - 40);
-            }
-            if(number == 50){
-                return "L";
-            }
-            if(number > 50 && number < 90){
-                return "L" + convertToRomanNumber(number - 50);
-            }
-            if(number == 90){
-                return "XC";
-            }
-            if(number > 90 && number < 100){
-                return "XC" + convertToRomanNumber(number - 90);
-            }
-            if(number == 100){
-                return "C";
-            }
-            if(number > 100 && number < 400){
-                return "C" + convertToRomanNumber(number - 100);
-            }
-            if(number == 400){
-                return "CD";
-            }
-            if(number > 400 && number < 500){
-                return "CD" + convertToRomanNumber(number - 400);
-            }
-            if(number == 500){
-                return "D";
-            }
-            if(number > 500 && number < 900){
-                return "D" + convertToRomanNumber(number - 500);
-            }
-            if(number == 900){
-                return "CM";
-            }
-            if(number > 900 && number < 1000){
-                return "CM" + convertToRomanNumber(number - 900);
-            }
-            if(number == 1000){
-                return "M";
-            }
-            if(number > 1000 && number < 3000){
-                return "M" + convertToRomanNumber(number - 1000);
-            }
-            if(number > 3000){
-                throw new IllegalArgumentException("Number too big");
-            }
-        } 
-        return null;
+        } else if (number > 3000) {
+            throw new IllegalArgumentException("Number too big");
+        } else {
+            StringBuilder romainNumber = new StringBuilder();
+            convertToRoman(number, romainNumber);
+            return romainNumber.toString();
+
+        }
     }
 
-    public String convertOneToFour(int number){
-        String result = "";
-        for(int i = 0; i < number; i++){
-            result += "I";
-        }
-        return result;
-    }
+    public void convertToRoman(int number, StringBuilder romainNumber) {
+        if (number == 0) {
+            return;
+        } else {
+            for (int i = 0; i < VALUES.length; i++) {
+                if (number >= VALUES[i]) {
+                    romainNumber.append(SYMBOLS[i]);
+                    convertToRoman(number - VALUES[i], romainNumber);
+                    return;
+                }
 
-    public String convertFiveToNine(int number){
-        String result = "V";
-        for(int i = 5; i < number; i++){
-            result += "I";
+            }
         }
-        return result;
+
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,220 +5,214 @@ import org.example.RomainConverter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
-
 public class MainTest {
 
-
     @Test
-    public void RomainConverter(){
+    public void RomainConverter() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(1);
+        String value = convertTest.toRomanNumerals(1);
 
         Assertions.assertEquals("I", value);
     }
 
     @Test
-    public void RomainConverter2(){
+    public void RomainConverter2() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(2);
+        String value = convertTest.toRomanNumerals(2);
 
         Assertions.assertEquals("II", value);
     }
 
     @Test
-    public void RomainConverter3(){
+    public void RomainConverter3() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(3);
+        String value = convertTest.toRomanNumerals(3);
 
         Assertions.assertEquals("III", value);
     }
 
     @Test
-    public void RomainConverter4(){
+    public void RomainConverter4() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(4);
+        String value = convertTest.toRomanNumerals(4);
 
         Assertions.assertEquals("IV", value);
     }
 
     @Test
-    public void RomainConverter5(){
+    public void RomainConverter5() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(5);
+        String value = convertTest.toRomanNumerals(5);
 
         Assertions.assertEquals("V", value);
     }
 
     @Test
-    public void RomainConverter6(){
+    public void RomainConverter6() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(6);
+        String value = convertTest.toRomanNumerals(6);
 
         Assertions.assertEquals("VI", value);
     }
 
     @Test
-    public void RomainConverter8(){
+    public void RomainConverter8() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(8);
+        String value = convertTest.toRomanNumerals(8);
 
         Assertions.assertEquals("VIII", value);
     }
 
     @Test
-    public void RomainConverter9(){
+    public void RomainConverter9() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(9);
+        String value = convertTest.toRomanNumerals(9);
 
         Assertions.assertEquals("IX", value);
     }
 
     @Test
-    public void RomainConverter10(){
+    public void RomainConverter10() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(10);
+        String value = convertTest.toRomanNumerals(10);
 
         Assertions.assertEquals("X", value);
     }
 
     @Test
-    public void RomainConverter11(){
+    public void RomainConverter11() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(11);
+        String value = convertTest.toRomanNumerals(11);
 
         Assertions.assertEquals("XI", value);
     }
 
     @Test
-    public void RomainConverter38(){
+    public void RomainConverter38() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(38);
+        String value = convertTest.toRomanNumerals(38);
 
         Assertions.assertEquals("XXXVIII", value);
     }
 
     @Test
-    public void RomainConverter40(){
+    public void RomainConverter40() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(40);
+        String value = convertTest.toRomanNumerals(40);
 
         Assertions.assertEquals("XL", value);
     }
 
     @Test
-    public void RomainConverter44(){
+    public void RomainConverter44() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(41);
+        String value = convertTest.toRomanNumerals(41);
 
         Assertions.assertEquals("XLI", value);
     }
 
     @Test
-    public void RomainConverter50(){
+    public void RomainConverter50() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(50);
+        String value = convertTest.toRomanNumerals(50);
 
         Assertions.assertEquals("L", value);
     }
 
     @Test
-    public void RomainConverter69(){
+    public void RomainConverter69() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(69);
+        String value = convertTest.toRomanNumerals(69);
 
         Assertions.assertEquals("LXIX", value);
     }
 
     @Test
-    public void RomainConverter99(){
+    public void RomainConverter99() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(99);
+        String value = convertTest.toRomanNumerals(99);
 
         Assertions.assertEquals("XCIX", value);
     }
 
     @Test
-    public void RomainConverter100(){
+    public void RomainConverter100() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(100);
+        String value = convertTest.toRomanNumerals(100);
 
         Assertions.assertEquals("C", value);
     }
 
     @Test
-    public void RomainConverter119(){
+    public void RomainConverter119() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(119);
+        String value = convertTest.toRomanNumerals(119);
 
         Assertions.assertEquals("CXIX", value);
     }
 
     @Test
-    public void RomainConverter270(){
+    public void RomainConverter270() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(200);
+        String value = convertTest.toRomanNumerals(200);
 
         Assertions.assertEquals("CC", value);
     }
 
     @Test
-    public void RomainConverter400(){
+    public void RomainConverter400() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(400);
+        String value = convertTest.toRomanNumerals(400);
 
         Assertions.assertEquals("CD", value);
     }
 
     @Test
-    public void RomainConverter500(){
+    public void RomainConverter500() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(500);
+        String value = convertTest.toRomanNumerals(500);
 
         Assertions.assertEquals("D", value);
     }
 
     @Test
-    public void RomainConverter987(){
+    public void RomainConverter987() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(987);
+        String value = convertTest.toRomanNumerals(987);
 
         Assertions.assertEquals("CMLXXXVII", value);
     }
 
     @Test
-    public void RomainConverter1259(){
+    public void RomainConverter1259() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(1259);
+        String value = convertTest.toRomanNumerals(1259);
 
         Assertions.assertEquals("MCCLIX", value);
     }
 
     @Test
-    public void RomainConverter2999(){
+    public void RomainConverter2999() {
         RomainConverter convertTest = new RomainConverter();
-        String value = convertTest.convertToRomanNumber(2999);
+        String value = convertTest.toRomanNumerals(2999);
 
         Assertions.assertEquals("MMCMXCIX", value);
     }
 
     @Test
-    public void RomainConverter3100(){
+    public void RomainConverter3100() {
         RomainConverter convertTest = new RomainConverter();
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> convertTest.convertToRomanNumber(3100));
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> convertTest.toRomanNumerals(3100));
         assertEquals("Number too big", exception.getMessage());
-        
+
     }
 
     @Test
-    public void RomainConverter0(){
+    public void RomainConverter0() {
         RomainConverter convertTest = new RomainConverter();
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> convertTest.convertToRomanNumber(0));
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> convertTest.toRomanNumerals(0));
         assertEquals("Number too small", exception.getMessage());
-        
+
     }
-
-
-
 
 }
