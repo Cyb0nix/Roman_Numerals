cab923a4a405892e3bc7af5e9e38c8caf215f1a0
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2023-07-30 00:24:50 +0200

    Added Demo in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // New instance of RomainConverter
+        RomainConverter converter = new RomainConverter();
+
+        // convert to Roman Numerals
+        int arabicNumber = converter.toArabicNumerals("LXIX");
+
+        // print result
+        System.out.println("LXIX -> " + arabicNumber);
+
+
+
+        // convert to Arabic Numerals
+        String romanNumber = converter.toRomanNumerals(69);
+
+        // print result
+        System.out.println("69 -> " + romanNumber);
+
+
     }
 }
